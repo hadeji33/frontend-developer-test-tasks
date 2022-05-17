240e03c8f5cc7cef8a5f309ded768a1ee0e2e4eb
hadeji33 <lairgold@yandex.ru>
2022-05-17 22:04:55 +0300

    task 2 add prop types checking

--- js_task/modules.js
+++ js_task/modules.js
@@ -1,6 +1,5 @@
 const createModules = function() {
     modulesStorage = {}
-    dependenciesStorage = []
 
     define = function(moduleName, dependencies = [], declarationFunction ) {
         modulesStorage[moduleName] = {
@@ -9,12 +8,26 @@ const createModules = function() {
             dependencies: dependencies,
             value: null
         }
-        dependenciesStorage = dependenciesStorage.concat(dependencies)
     }
 
     require = function(dependencies = [], successCallback) {
         const context = [];
+
+        if(!Array.isArray(dependencies) || dependencies.length == 0) {
+            successCallback()
+            return
+        }
+
         const promises = dependencies.map(dep => {
+            if(typeof dep !== 'string') {
+                console.error('Bad-format dependencies property: ', `${typeof dep} is not a string`);
+                return null
+            }
+            if(!modulesStorage[dep]) {
+                console.error('Not defined: ', `dependency ${dep} was not defined. Use define function first`);
+                return null
+            }
+            
             return new Promise((resolve) => {
                 modulesStorage[dep].df((a) => {
                     if(!modulesStorage[dep].value) {
@@ -28,10 +41,11 @@ const createModules = function() {
         Promise.allSettled(promises).then(
             (results) => results.forEach(
                 (result) => { 
-                    context.push(modulesStorage[result.value].value)
+                    context.push(modulesStorage[result.value]?.value)
                 }
             )
         ).then(() => { 
+            console.log(context);
             successCallback.apply(null, context);
         })
     }
